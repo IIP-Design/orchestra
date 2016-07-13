da72d2f5c100c874f2a41c113bdcee6d01656519
Nathan Kleekamp <nkleekamp@gmail.com>
2016-07-13 07:47:18 -0400

    added test template to speed up writing tests

---
+++ test/migrations/test-template.js
@@ -0,0 +1,11 @@
+const common = require('../common');
+const knex = common.knex;
+const expect = common.expect;
+const config = common.config;
+
+describe('', () => {
+  const vari = {};
+
+  it('', () => {
+  });
+});
