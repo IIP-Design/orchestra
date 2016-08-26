c272cc07b1d0b6b5e7ce1a2ac44b450e22cdc52b
Nathan Kleekamp <nkleekamp@gmail.com>
2016-08-26 16:14:08 -0400

    Intial attempt at extending the wordpress library

--- test/application/client_object.js
+++ test/application/client_object.js
@@ -1,91 +1,78 @@
+// NPM module imports
 const wordpress = require('wordpress');
 const _ = require('underscore');
 const rewire = require('rewire');
+
+// Project testing modules
 const common = require('../common');
 const knex = common.knex;
 const expect = common.expect;
 const config = common.config.websites;
-const testConfig = require('../../docs/config-example').test.websites;
+const testConfig = require('../../docs/config-example').test.websites[0];
 const clients = rewire('../../lib/application/client');
 
+// Global variables
+let keys = ['url', 'apiUrl', 'getUsername', 'getPassword', 'getConnection', 'setConnection', 'getLastUpdated', 'setLastUpdated', 'getResources', 'update'];
 
-describe('- Create the client object -', () => {
+
+
+
+describe('- Create the client object with clientConstructor -', () => {
   const clientConstructor = clients.__get__('clientConstructor');
-  const wpClientConstructor = clients.__get__('wpClientConstructor');
-  const getWpResources = clients.__get__('getWpResources');
-  const args = { config: testConfig[0], logger: console };
-  let keys = ['url', 'apiUrl', 'isConnected', 'getUsername', 'getPassword', 'getLastUpdated', 'setLastUpdated', 'update'];
 
   it('should pass if it returns an object', () => {
-    const test = clientConstructor(args);
-    expect(_.isObject(test)).to.be.true;
+    const client = clientConstructor(testConfig, console);
+    expect(_.isObject(client)).to.be.true;
   });
 
 
 
   it('should have all of the provided keys', () => {
-    const test = clientConstructor(args)
-    expect(test).to.have.all.keys(keys);
-  });
-
-
-
-  it('should return false if it does not have a connection object', () => {
-    const test = clientConstructor(args);
-    expect(test.isConnected()).to.be.false;
+    const client = clientConstructor(testConfig, console)
+    expect(client).to.have.all.keys(keys);
   });
 
 
 
   it('should return the username', () => {
-    const test = clientConstructor(args);
-    expect(test.getUsername()).to.equal('wp_username');
+    const client = clientConstructor(testConfig, console);
+    expect(client.getUsername()).to.equal('wp_username');
   });
 
 
 
   it('should return the password', () => {
-    const test = clientConstructor(args);
-    expect(test.getPassword()).to.equal('wp_password');
+    const client = clientConstructor(testConfig, console);
+    expect(client.getPassword()).to.equal('wp_password');
   });
 
 
 
   it('should return the lastUpdated', () => {
-    const test = clientConstructor(args);
-    expect(test.getPassword()).to.equal('wp_password');
+    const client = clientConstructor(testConfig, console);
+    expect(client.getPassword()).to.equal('wp_password');
   });
 
 
 
   it('should get and set last updated', () => {
     const now = Date.now();
-    const test = clientConstructor(args);
-    expect(test.setLastUpdated()).to.be.closeTo(now, 10);
-    expect(test.getLastUpdated()).to.be.closeTo(now, 10);
+    const client = clientConstructor(testConfig, console);
+    expect(client.setLastUpdated()).to.be.closeTo(now, 10);
+    expect(client.getLastUpdated()).to.be.closeTo(now, 10);
   })
+});
 
 
 
-  it('should throw an error if there if client.connection is undefined', () => {
-    const test = clientConstructor(args);
-    expect(() => test.update()).to.throw(Error);
-  });
 
-
+describe('- Create the client object with wpClientConstructor -', () => {
+  const wpClientConstructor = clients.__get__('wpClientConstructor');
 
   it('should return client, with a WP connection', () => {
-    const test = wpClientConstructor(args);
-    keys.push('connection');
-    expect(test).to.have.all.keys(keys);
-    expect(test.connection).to.be.an.instanceof(wordpress.Client);
-    expect(test.connection).to.have.property('getPosts');
-    expect(test.connection).to.have.ownProperty('getResources');
-  });
-
-
-
-  it('should return a promise', () => {
-    const test = getWpResources(args);
+    const client = wpClientConstructor(testConfig, console);
+    expect(client).to.have.all.keys(keys);
+    expect(client.getConnection()).to.be.an.instanceof(wordpress.Client);
+    expect(client.getConnection()).to.have.property('getPosts');
   });
 });
