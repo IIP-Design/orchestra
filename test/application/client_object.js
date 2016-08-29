2072fbf34c859993b5aac6c2e3d01f9461281be8
Nathan Kleekamp <nkleekamp@gmail.com>
2016-08-29 16:07:23 -0400

    added some tests, expanded clientConstructor

--- test/application/client_object.js
+++ test/application/client_object.js
@@ -12,55 +12,111 @@ const testConfig = require('../../docs/config-example').test.websites[0];
 const clients = rewire('../../lib/application/client');
 
 // Global variables
-let keys = ['url', 'apiUrl', 'getUsername', 'getPassword', 'getConnection', 'setConnection', 'getLastUpdated', 'setLastUpdated', 'getResources', 'update'];
+let keys = [
+  'url',
+  'apiUrl',
+  'getUsername',
+  'getPassword',
+  'getConnection',
+  'setConnection',
+  'getLastUpdated',
+  'setLastUpdated',
+  'getResources',
+  'update'
+];
 
 
 
 
-describe('- Create the client object with clientConstructor -', () => {
+describe('- Extend the wordpress.Client object to add a custom fetchResources method -', () => {
+  const getExtendedWpConnection = clients.__get__('getExtendedWpConnection');
+
+  it('should return a Wordpress connection instance', () => {
+    const connection = getExtendedWpConnection(testConfig);
+    expect(connection.constructor.name).to.equal('WP');
+  });
+
+
+
+  it('should have a fetchResources method, connection.fetchResources', () => {
+    const connection = getExtendedWpConnection(testConfig);
+    const methods = _.functions(connection);
+    expect(methods).to.contain('fetchResources');
+  });
+
+
+
+  it('should return a promise, connection.fetchResources', () => {
+    const connection = getExtendedWpConnection(testConfig);
+    expect(connection.fetchResources()).to.be.a('promise');
+  });
+});
+
+
+
+
+describe('- Create the client object with clientConstructor factory function-', () => {
   const clientConstructor = clients.__get__('clientConstructor');
 
-  it('should pass if it returns an object', () => {
+  it('should pass if it returns an object, clientConstructor', () => {
     const client = clientConstructor(testConfig, console);
     expect(_.isObject(client)).to.be.true;
   });
 
 
 
-  it('should have all of the provided keys', () => {
-    const client = clientConstructor(testConfig, console)
+  it('should have all of the provided keys, clientConstructor', () => {
+    const client = clientConstructor(testConfig, console);
     expect(client).to.have.all.keys(keys);
   });
 
 
 
-  it('should return the username', () => {
+  it('should return the __username property, client.getUsername',() => {
     const client = clientConstructor(testConfig, console);
     expect(client.getUsername()).to.equal('wp_username');
   });
 
 
 
-  it('should return the password', () => {
+  it('should return the __password property, client.getPassword', () => {
     const client = clientConstructor(testConfig, console);
     expect(client.getPassword()).to.equal('wp_password');
   });
 
 
 
-  it('should return the lastUpdated', () => {
+  it('should return an error if setConnection param is not an object, client.setConnection', () => {
     const client = clientConstructor(testConfig, console);
-    expect(client.getPassword()).to.equal('wp_password');
+    const connection = true;
+    expect(() => client.setConnection(connection)).to.throw(Error, /Connection must be an object/);
+  });
+
+
+  it('should get and set the __connection property, client.getConnection', () => {
+    const client = clientConstructor(testConfig, console);
+    const connection = {
+      test: () => 'hi'
+    };
+    client.setConnection(connection);
+    expect(client.getConnection()).to.eql(connection);
   });
 
 
 
-  it('should get and set last updated', () => {
+  it('should get and set last updated, client.setLastUpdated/client.getLastUpdated', () => {
+    const client = clientConstructor(testConfig, console);
     const now = Date.now();
-    const client = clientConstructor(testConfig, console);
     expect(client.setLastUpdated()).to.be.closeTo(now, 10);
     expect(client.getLastUpdated()).to.be.closeTo(now, 10);
   })
+
+
+
+  it('should throw an error if the connection is not set, client.getResources', () => {
+    const client = clientConstructor(testConfig, console);
+    expect(() => client.getResources()).to.throw(Error, /Client connection is undefined/);
+  });
 });
 
 
